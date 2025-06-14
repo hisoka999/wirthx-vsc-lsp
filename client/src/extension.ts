82166c12eea281f2493ed7d64132504bdd0e5ab3
hisoka999 <hisoka999@gmail.com>
2025-06-14 15:04:14 +0200

    initial commit

---
+++ client/src/extension.ts
@@ -0,0 +1,62 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	const serverModule = '/home/stefan/projects/wirthx/cmake-build-relwithdebinfo/wirthx';
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: { command: serverModule, args: ["--rtl", "/home/stefan/projects/wirthx/rtl", "--lsp"] },
+		debug: {
+			command: serverModule,
+			args: ["--rtl", "/home/stefan/projects/wirthx/rtl", "--lsp"]
+
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'pascal' }],
+		diagnosticCollectionName: "wirthx",
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/*.pas')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'WirthXLanguageServer',
+		'WirthX Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
